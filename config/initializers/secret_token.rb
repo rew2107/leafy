b3f1473a867d579e8dd8d605464b493aa3df325e
Nolan Frausto <nrfrausto@gmail.com>
2013-05-26 14:32:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leafy::Application.config.secret_token = '76d972387ecb4d9d90671ba513e645bfad91e3a4658af928c21e4a5f85d506b0e27460ed31e5c7058c3dc96ce40713421b2c3cc37399134571c159c5ed155ce1'
