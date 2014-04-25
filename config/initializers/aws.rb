aaa4e7a7d1ac23d1ae9c3769144cbf01a9e9aa7d
Nolan Frausto <nrfrausto@gmail.com>
2014-04-25 10:31:55 -0400

    add aws access keys

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS.config({
-  :access_key_id => 'AKIAJPR4VESDA3NDTU5Q',
-  :secret_access_key => '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'
+  :access_key_id => 'AKIAJKVQMLQLEQFHVZ5A',
+  :secret_access_key => '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'
 })
